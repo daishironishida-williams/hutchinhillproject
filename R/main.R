a2410d8faae6cfc64d878590d2e507e6beb2bdb8
Daishiro Nishida <dn6@williams.edu>
2016-01-31 18:39:00 -0500

    Changes to pass the check
    not complete

--- R/main.R
+++ R/main.R
@@ -1,12 +1,12 @@
 allprofs <- read.csv("Data.csv")
 
-yearorder <- order( allprofs$year_of_grad )
-print(allprofs[yearorder,])
+year_order <- order( allprofs$year_of_grad )
+print(allprofs[year_order,])
 
-departmentorder <- order( allprofs$department )
-print(allprofs[departmentorder,])
+department_order <- order( allprofs$department )
+print(allprofs[department_order,])
 
-gradyears <- allprofs$year_of_grad
-meangradyear <- mean(gradyears)
-meanage <- 2014 - mean(gradyears) + 22
-print(meanage)
+grad_years <- allprofs$year_of_grad
+mean_grad_year <- mean(grad_years)
+mean_age <- 2014 - mean(grad_years) + 22
+print(mean_age)
