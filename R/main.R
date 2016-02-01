39b805cbfff5f42f12be33fc69fa8e8228205ffb
Daishiro Nishida <dn6@williams.edu>
2016-01-31 22:06:25 -0500

    Made some plots

--- R/main.R
+++ R/main.R
@@ -6,7 +6,21 @@ print(allprofs[year_order,])
 department_order <- order( allprofs$department )
 print(allprofs[department_order,])
 
-grad_years <- allprofs$year_of_grad
-mean_grad_year <- mean(grad_years)
-mean_age <- 2014 - mean(grad_years) + 22
+ages <- 2014 - allprofs$year_of_grad + 22
+
+allprofs <- cbind(allprofs, ages)
+
+print(allprofs)
+
+mean_age <- mean(ages)
 print(mean_age)
+
+dep_label <- c("AFR", "AMST", "ANTH", "ARAB", "ARTS", "ARTH", "ASTR", "BIOL", "CHEM", "CHIN",
+               "CLAS", "COMP", "CSCI", "DANC", "ECON", "ENGL", "ENVI", "GEOS", "GERM", "HIST",
+               "JAPN", "LATS", "LEAD", "MAST", "MATH", "MUS", "PHIL", "PHED", "PHYS", "PSCI",
+               "PSYC", "REL", "RL", "RUSS", "SOC", "STAT", "THEA", "WGSS")
+
+ggplot(allprofs, aes(department)) + geom_bar() + scale_x_discrete(labels= dep_label) + theme(
+  axis.text.x = element_text(angle=90, vjust=1))
+ggplot(allprofs, aes(department, ages)) + geom_boxplot() + scale_x_discrete(labels= dep_label) + theme(
+  axis.text.x = element_text(angle=90, vjust=1))
