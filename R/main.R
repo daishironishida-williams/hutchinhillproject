44358c976ca620e37c7d870099bc799cccf62885
Daishiro Nishida <dn6@williams.edu>
2016-02-01 14:26:47 -0500

    Added division and last degree

--- R/main.R
+++ R/main.R
@@ -1,12 +1,12 @@
 allprofs <- read.csv("Data.csv")
 
-year_order <- order( allprofs$year_of_grad )
+year_order <- order( allprofs$year_of_BA )
 print(allprofs[year_order,])
 
 department_order <- order( allprofs$department )
 print(allprofs[department_order,])
 
-ages <- 2014 - allprofs$year_of_grad + 22
+ages <- 2014 - allprofs$year_of_BA + 22
 
 allprofs <- cbind(allprofs, ages)
 
@@ -20,7 +20,9 @@ dep_label <- c("AFR", "AMST", "ANTH", "ARAB", "ARTS", "ARTH", "ASTR", "BIOL", "C
                "JAPN", "LATS", "LEAD", "MAST", "MATH", "MUS", "PHIL", "PHED", "PHYS", "PSCI",
                "PSYC", "REL", "RL", "RUSS", "SOC", "STAT", "THEA", "WGSS")
 
-ggplot(allprofs, aes(department)) + geom_bar() + scale_x_discrete(labels= dep_label) + theme(
-  axis.text.x = element_text(angle=90, vjust=1))
+div_label <- c("Div. I", "Div. II", "Div. III", "PE")
+
+ggplot(allprofs, aes( ages ) ) + geom_histogram(
+  binwidth = 1, aes( fill = division ) ) + scale_fill_discrete(labels= div_label)
 ggplot(allprofs, aes(department, ages)) + geom_boxplot() + scale_x_discrete(labels= dep_label) + theme(
   axis.text.x = element_text(angle=90, vjust=1))
