866b4429bf96e7006ce68f5200d65a34c0c416c2
Daishiro Nishida <dn6@williams.edu>
2016-02-02 22:54:39 -0500

    Added data

---
+++ tests/testthat/test_get_mean_output.R
@@ -0,0 +1,7 @@
+test_that("the mean and the standard deviation are correct", {
+  get_mean()
+  expect_equal( div1_mean, 50.4503816793893 )
+  expect_equal( div2_mean, 48.8620689655172 )
+  expect_equal( div3_mean, 48.6756756756757 )
+  expect_equal( pe_mean, 45.1212121212121)
+})
