56dc47605ce093426d36c37b915dbb8b557f18c1
Daishiro Nishida <dn6@williams.edu>
2016-02-03 00:17:41 -0500

    Ready for submission

--- tests/testthat/test_analyse_data_output.R
+++ tests/testthat/test_analyse_data_output.R
@@ -1,5 +1,10 @@
+library(hutchinhillproject)
+context("Mean and sd of allprofs")
+
+data(allprofs)
+analyse_data( allprofs )
+
 test_that("the mean and the standard deviation are correct", {
-  read_data()
   expect_equal( mean_age, 49.0255754475703 )
   expect_equal( deviation, 12.1557988953172 )
 })
--- tests/testthat/test_get_mean_output.R
+++ tests/testthat/test_get_mean_output.R
@@ -1,5 +1,10 @@
+library(hutchinhillproject)
+context("Info on each division")
+
+analyse_data( allprofs )
+get_mean()
+
 test_that("the mean and the standard deviation are correct", {
-  get_mean()
   expect_equal( div1_mean, 50.4503816793893 )
   expect_equal( div2_mean, 48.8620689655172 )
   expect_equal( div3_mean, 48.6756756756757 )
